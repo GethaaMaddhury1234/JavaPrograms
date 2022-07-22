c5b398e604e1d24218ac29560106ce85d7f22108
GethaaMaddhury <gethaa1234@sasi.ac.in>
2022-07-22 02:24:15 -0700

    file added

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+class Arithmetic
+{
+	public static void main(string args[])
+	{
+		int a=10;
+		int b=20;
+		int c=a-b;
+		int e=a*b;
+		int f=a/b;
+		System.out.println("The addition of a and b is "+a+b);
+		System.out.println("The subtraction of a and b is "+c);
+		System.out.println("The multiplication of a and b is "+e);
+		System.out.println("The division of a and b is "+f);
+	}
+}
